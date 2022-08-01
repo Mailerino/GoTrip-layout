ae09f2e7038a9402d60e310d7fa1d3401003cbff
Vector Miller <victorglebovv@yandex.ru>
2022-08-01 18:07:29 +0500

    Landing done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+const owl = $('.owl-carousel');
+
+owl.owlCarousel(
+  {
     center: true,
     loop: true,
     margin: 30,
     startPosition: 1,
     items: 3,
-  });
-});
+  }
+);
+
+$('.slider__button--prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+})
+
+$('.slider__button--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
