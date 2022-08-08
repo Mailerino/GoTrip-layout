7944f25655dc9b6caa86f6301e7f3ba93d1e3ba8
Vector Miller <victorglebovv@yandex.ru>
2022-08-08 23:08:25 +0500

    Responsive until 679px done

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
+// Owl Carousel
 const owl = $('.owl-carousel');
 
 owl.owlCarousel(
-  {
-    center: true,
-    loop: true,
-    margin: 30,
-    startPosition: 1,
-    items: 3,
-  }
-);
+{
+  center: true,
+  loop: true,
+  margin: 30,
+  startPosition: 1,
+  items: 3,
+  responsive : {
+    320 : {
+      margin: 20,
+      items: 1,
+      center: false,
+    },
+    600 : {
+      items: 3,
+      center: true,
+    },
+    1182 : {
+      items: 3,
+      margin: 30,
+    },
+  },
+});
+
+
 
 $('.slider__button--prev').click(function() {
     owl.trigger('prev.owl.carousel');
@@ -17,3 +34,14 @@ $('.slider__button--prev').click(function() {
 $('.slider__button--next').click(function() {
     owl.trigger('next.owl.carousel');
 })
+
+// Header Navigation Button
+const nav_button = document.querySelector('.nav__toggle');
+const menu_icon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
+
+nav_button.onclick = function(){
+  nav.classList.toggle('nav--mobile');
+  menu_icon.classList.toggle('menu-icon--active');
+  document.body.classList.toggle('no-scroll');
+}
